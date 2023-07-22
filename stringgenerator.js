ded71c6eb4ea509739cba7617cffd329250aa998
hyperClink <45831360+hyperClink@users.noreply.github.com>
2023-07-23 01:03:28 +0300

    Update stringgenerator.js

--- stringgenerator.js
+++ stringgenerator.js
@@ -13,6 +13,6 @@ for (let i = 1; i <= 438; i++) {
   var omega = reader2cache.databases["reader2viewer"].transaction("page", 'readonly').objectStore("page").get(book_id.toString() + ":" + i +":0")
 	omega.onsuccess = function(){console.log(omega.result.slices.join()); pagearr.push(omega.result.slices.join())}
 	}
-console.log(pagearr) 
+console.log(pagearr.join()) 
 }
 printstores()
